e2fcc5221f5f91cd5c40dff5c90d0e890215293b
Niranjan Kumar K S <niranjankumar26081998@gmail.com>
2020-02-12 16:19:15 +0530

    Weekday class added

--- src/edu/amrita/timetable/MapClass.java
+++ src/edu/amrita/timetable/MapClass.java
@@ -9,5 +9,7 @@ public class MapClass
 {
 	public static List<TimeSlot> TimeSlotList=new ArrayList<TimeSlot>();
 	public static Map<Integer,List<TimeSlot> > TimeSlotGroup = new TreeMap<Integer,List<TimeSlot> >();
+	public static List<WeekDay> WeekDayList=new ArrayList<WeekDay>();
+	public static Map<Integer,List<WeekDay> > WeekDayGroup = new TreeMap<Integer,List<WeekDay> >();
 	
 }
---
+++ src/edu/amrita/timetable/WeekDay.java
@@ -0,0 +1,20 @@
+package edu.amrita.timetable;
+
+public class WeekDay 
+{
+	private int ID;
+	private String description;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
