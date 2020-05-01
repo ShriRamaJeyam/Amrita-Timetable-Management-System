c5bcc83b5dbbe31264962fe09221f9eba1b34a3c
Niranjan Kumar K S Niranjan Kumar K S <nrans@amazon.com>
2020-05-01 14:59:57 +0530

    test

--- routes/api/Departments/index.js
+++ routes/api/Departments/index.js
@@ -59,4 +59,5 @@ router.post("/list",async function(req,res){
   }
 });
 
+
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.use("/Courses",require("./Courses/index"));
+fs.readdir("./routes/api",function(err,items){
+    items.forEach(dir => {
+        if(dir !== "index.js")
+        {
+            router.use("/"+dir,require("./"+dir+"/index"));
+        }
+    });
+})
 
 module.exports = router;
