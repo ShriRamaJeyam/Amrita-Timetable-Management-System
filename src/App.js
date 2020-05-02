6c7f237be88e2c12e85bbb9301ea8abe1e831e52
Niranjan Kumar K S Niranjan Kumar K S <nrans@amazon.com>
2020-05-02 20:03:39 +0530

    next

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React,{Fragment} from 'react';
+import { Typography } from '@material-ui/core';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <Fragment>
+    <div style={{width:"100%",padding:"10px",backgroundColor:"white"}}>
+      <a href="/">
+        <img style={{height:"80px"}} alt="amrita" src="/logo.png">
+      </img></a>
+    </div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload1.
         </p>
         <a
           className="App-link"
@@ -20,6 +27,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Fragment>
   );
 }
 
